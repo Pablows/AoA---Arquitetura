f24fb89b9f0391aba723f8cf655e184ae0f69b73
Pablows <pablomarreiro@gmail.com>
2021-04-28 15:02:44 -0300

    Adicionar arquivos de projeto.

---
+++ projeto2/Models/cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace projeto2.Models
+{
+    public class cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
---
+++ projeto2/Models/contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+
+
+
+namespace projeto2.Models
+{
+    public class contexto: DbContext
+    {
+        public DbSet<cliente> Cliente { get; set; }
+    }
+}
